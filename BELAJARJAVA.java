8f0f16efc919491cc630ce971de5d301609891e6
Nahlul Jamil Azrirrahman <nahluljamil.a@gmail.com>
2024-09-24 16:33:31 +0700

    tes sampai sini

--- BELAJARJAVA.java
+++ BELAJARJAVA.java
@@ -1,15 +1,5 @@
 public class BELAJARJAVA {
   public static void main(String[] args) {
-    System.out.print(10 + 10);
-    System.out.print(" + ");
-    System.out.print(5 + 5);
-    System.out.println(" = ");
-    System.out.println("30");
-    System.out.println("MUDAH SEKALI BUKAN????");
-    System.out.print("Selamat");
-    System.out.println(" Belajar Java");
-    //jangan di otak atik
-    int satu = 12;
-    System.out.println(satu);
+  System.out.println("Hello, World!");
   }
-}
+  }
